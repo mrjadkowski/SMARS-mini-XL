a2cff316b2468a501e386e47fe1389cc23cd13cd
mrjadkowski <81754963+mrjadkowski@users.noreply.github.com>
2021-04-05 21:13:33 -0400

    Added motor initialization

--- code.py
+++ code.py
@@ -2,8 +2,28 @@ import board
 import time
 import busio
 import adafruit_vl53l0x
+import pwmio
+from adafruit_motor import motor
 
-#initialize i2c and time of flight sensor
+# set up PWM pins for left motor
+PWM_PIN_A = board.D7
+PWM_PIN_B = board.D8
+
+# set up PWM pins for right motor
+PWM_PIN_C = board.D9
+PWM_PIN_D = board.D10
+
+# set up left motor
+pwm_a = pwmio.PWMOut(PWM_PIN_A, frequency=50)
+pwm_b = pwmio.PWMOut(PWM_PIN_B, frequency=50)
+leftmotor = motor.DCMotor(pwm_a, pwm_b)
+
+# set up right motor
+pwm_c = pwmio.PWMOut(PWM_PIN_C, frequency=50)
+pwm_d = pwmio.PWMOut(PWM_PIN_D, frequency=50)
+rightmotor = motor.DCMotor(pwm_c, pwm_d)
+
+# initialize i2c and time of flight sensor
 i2c = busio.I2C(board.SCL, board.SDA)
 tof = adafruit_vl53l0x.VL53L0X(i2c)
 
