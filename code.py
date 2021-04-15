6a6c35d4e883cd52d7a051feb6b0e6545dd2995c
mrjadkowski <81754963+mrjadkowski@users.noreply.github.com>
2021-04-15 14:46:37 -0400

    Ported to run on RP2040 board
    Wired IAW 2.x.x wiring diagram

--- code.py
+++ code.py
@@ -1,4 +1,4 @@
-# this is designed to run on an adafruit QT Py board running Circuit Python 6.1.0
+# this is designed to run on an adafruit Feather RP2040 board running Circuit Python 6.2.0
 # using a VL53L0X ToF sensor and a DRV8833 motor driver.
 
 import board
@@ -6,17 +6,17 @@ import time
 import busio
 import adafruit_vl53l0x
 import pwmio
-import touchio
+from digitalio import DigitalInOut, Direction
 from adafruit_debouncer import Debouncer
 from adafruit_motor import motor
 
 # set up PWM pins for left motor
-PWM_PIN_A = board.D7
-PWM_PIN_B = board.D8
+PWM_PIN_A = board.D13 # yellow wire
+PWM_PIN_B = board.D12 # green wire
 
 # set up PWM pins for right motor
-PWM_PIN_C = board.D9
-PWM_PIN_D = board.D10
+PWM_PIN_C = board.D11 # white wire
+PWM_PIN_D = board.D10 # blue wire
 
 # set up left motor
 pwm_a = pwmio.PWMOut(PWM_PIN_A, frequency=50)
@@ -32,9 +32,9 @@ rightmotor = motor.DCMotor(pwm_c, pwm_d)
 i2c = busio.I2C(board.SCL, board.SDA)
 tof = adafruit_vl53l0x.VL53L0X(i2c)
 
-# setup touch input
-touch = touchio.TouchIn(board.D6)
-touch_debounced = Debouncer(touch)
+# setup switch input
+switch = DigitalInOut(board.D24)
+switch.direction = Direction.INPUT
 
 # set up turn and drive parameters
 start_turn_distance = 100
@@ -58,9 +58,7 @@ while True:
     now = time.monotonic()
 
     # invert run value when touched
-    touch_debounced.update()
-    if touch_debounced.rose:
-        run_flag = ~run_flag
+    run_flag = switch.value
 
     # when start_turn_distance or less from an obstacle, set turn flag
     if tof.range <= start_turn_distance:
